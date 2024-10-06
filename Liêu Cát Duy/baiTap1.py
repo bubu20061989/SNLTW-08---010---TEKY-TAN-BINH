4bc9eaa39aa9e112f87410633269c9ac9ad67203
ricon7 <ronaldoricon509@gmail.com>
2024-10-06 09:35:44 +0700

    Cát Duy nộp bài baiTap1.py lần 2

--- Liêu Cát Duy/baiTap1.py
+++ Liêu Cát Duy/baiTap1.py
@@ -1,6 +1,5 @@
-dienNangkWCuaGiaDinh = float(input('' Nhập số điện năng tiêu thụ của 1 hộ gia đình Kw
-''))
-tien1Kw = float(input(''Nhập số tiền trên 1 Kw''))
-print(''Số tiền gia đình phải trả là:'',dienNangKwCuaGiaDinh*tien1Kw)    
+dienNangKWCuaGiaDinh = float(input(' Nhập số điện năng tiêu thụ của 1 hộ gia đình Kw'))
+tien1Kw = float(input('Nhập số tiền trên 1 Kw'))
+print('Số tiền gia đình phải trả là:',dienNangKWCuaGiaDinh*tien1Kw)    
 
 #CatDuy
