eaee90d6c45d7414f20af5d755f47e44a9ef8825
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-06 09:11:11 +0700

    123

---
+++ Thầy Viên/kienThucInput.py
@@ -0,0 +1,13 @@
+# 2 CÂU LỆNH INPUT
+
+# ----------------------------------------------------------------
+# khai báo biến = input("Chú thích")
+a = input("Hãy nhập tên của bạn")
+
+#Lưu ý: input luôn trả về dạng chữ
+
+a = float(input("Hãy nhập số a: "))
+
+b = input("Hãy nhập số a")
+float(b)
+
---
+++ Thầy Viên/kienthucKieuDuLieu.py
@@ -0,0 +1,11 @@
+# 3 CÂU LỆNH INPUT
+
+# string - str - Chuỗi
+string = "Hello, World!"
+# float - float - Số thập phân
+float_number = 3.14
+# integer - int - Số nguyên
+integer_number = 10
+# boolean - bool - true or false
+boolean_value = True
+
