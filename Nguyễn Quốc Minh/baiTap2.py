9048dc9b94a13502f14118aaea6f6f0863e2f650
skibiditoilet999 <minhdoraemon2301@gmail.com>
2024-10-06 09:53:10 +0700

    Quốc Minh nộp baiTap2 lần 1

---
+++ Nguyễn Quốc Minh/baiTap2.py
@@ -0,0 +1,7 @@
+a=int(input("Hãy nhập số nguyên: "))
+b=int(input("Hãy nhập số nguyên: "))
+c=int(input("Hãy nhập số nguyên: "))
+d=int(input("Hãy nhập số nguyên; "))
+e=int(input("Hãy nhập số nguyên: "))
+print("Tổng: ",a+b+c+d+e)
+print("Trung bình: ",(a+b+c+d+e)/5)
