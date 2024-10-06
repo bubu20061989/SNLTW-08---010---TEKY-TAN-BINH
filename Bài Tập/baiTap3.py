f34bf67164fd6eba1ba844916ecf48b9edf3c3c9
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-06 09:19:33 +0700

    thầy viên

---
+++ Bài Tập/baiTap3.py
@@ -0,0 +1,7 @@
+# Viết chương trình nhập vào số tiền VND cần đổi và tỷ giá đổi sang USD (số VND đổi được 1 USD). Chương trình sẽ tính và in ra số tiền USD tương ứng.
+
+# Ví dụ:
+
+# Nhập số tiền VND: 1,000,000
+# Nhập tỷ giá: 23,000
+# Kết quả: Số tiền tương ứng là 43.48 USD.
---
+++ Bài Tập/baiTap4.py
@@ -0,0 +1,9 @@
+# Viết chương trình nhập vào số tiền gửi ban đầu, lãi suất ngân hàng theo tháng (%), và số tháng gửi. Sau đó tính và in ra tổng số tiền sau khi hết thời gian gửi theo công thức lãi kép:
+# Số tiền sau = Số tiền ban đầu * ( 1 + lãi suất ) * số tháng
+
+# Ví dụ:
+
+# Nhập số tiền gửi ban đầu: 10,000,000 VND
+# Nhập lãi suất hàng tháng: 0.5%
+# Nhập số tháng gửi: 12
+# Kết quả: Tổng số tiền sau 12 tháng.
