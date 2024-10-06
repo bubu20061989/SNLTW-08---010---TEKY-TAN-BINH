38317ff75332e97941cd63d0f4821acffb5588cd
ricon7 <ronaldoricon509@gmail.com>
2024-10-06 09:28:44 +0700

    Liêu Cát Duy baiTap1 lần 1

---
+++ Liêu Cát Duy/baiTap1.py
@@ -0,0 +1,6 @@
+dienNangkWCuaGiaDinh = float(input('' Nhập số điện năng tiêu thụ của 1 hộ gia đình Kw
+''))
+tien1Kw = float(input(''Nhập số tiền trên 1 Kw''))
+print(''Số tiền gia đình phải trả là:'',dienNangKwCuaGiaDinh*tien1Kw)    
+
+#CatDuy
