7e966c866db1ca0f3dcf67c7548031268db35a88
skibiditoilet999 <minhdoraemon2301@gmail.com>
2024-10-06 09:33:51 +0700

    Quốc Minh nộp baiTap1

---
+++ Nguyễn Quốc Minh/baiTap1.py
@@ -0,0 +1,4 @@
+diennangtieuthu= float(input("hãy nhập số điện năng tiêu thụ: "))
+sotien1kgw= float(input("hãy nhập số tiền mỗi kgw tiêu thụ: "))
+
+print("số tiền phải trả là:",diennangtieuthu*sotien1kgw)
