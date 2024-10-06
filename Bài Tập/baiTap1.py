3cd08f77083afb43e26d2747ee180229b637ade5
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-06 09:09:41 +0700

    Update baiTap1.py

---
+++ Bài Tập/baiTap1.py
@@ -0,0 +1,9 @@
+# Nhập số điện năng tiêu thụ của 1 hộ gia đình KgW
+
+# Nhập số tiền trên 1 KgW
+
+# Tính số tiền gia đình đó cần phải trả
+
+# VD: Số điện năng tiêu thụ là 50
+#      Số tiền trên 1 KgW là 3000
+# => kết quả 50 * 3000
