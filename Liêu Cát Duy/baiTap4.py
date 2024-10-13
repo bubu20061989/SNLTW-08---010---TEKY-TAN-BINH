9a1d59fe6d4730cb5c3f5a28154676363bfc9d2a
ricon7 <ronaldoricon509@gmail.com>
2024-10-13 08:28:12 +0700

    Duy nộp baiTap4 lần 2

--- Liêu Cát Duy/baiTap4.py
+++ Liêu Cát Duy/baiTap4.py
@@ -2,5 +2,6 @@ soTienGui=float (input(' Bạn muốn gửi bao nhiêu tiền?'))
 lai=float (input(' Lãi :'))
 soThangGui=float (input(' Bạn muốn gửi bao nhiêu tháng?'))
 print(soTienGui*(1+lai)*soThangGui)
+print('tongSoTienSau12ThangLa:',soTienGui*(1+lai)*soThangGui)
 
 
