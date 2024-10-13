1e3676f6c4234da3ebd606d609c8c44b3a401115
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-13 09:48:30 +0700

    Create baiTap7.py

---
+++ Bài Tập/baiTap7.py
@@ -0,0 +1,23 @@
+# Viết một chương trình quản lý thời tiết với các yêu cầu sau:
+
+# Nhập dữ liệu nhiệt độ:
+
+# Yêu cầu người dùng nhập nhiệt độ của một ngày bất kỳ (dạng số thực, đơn vị là °C).
+# Phân loại thời tiết:
+
+# Dựa vào nhiệt độ nhập vào, phân loại và hiển thị trạng thái thời tiết như sau:
+# Nếu nhiệt độ trên 35°C: Thời tiết "Nóng bức".
+# Nếu 30°C <= nhiệt độ <= 35°C: Thời tiết "Nóng".
+# Nếu 25°C <= nhiệt độ < 30°C: Thời tiết "Ấm".
+# Nếu 20°C <= nhiệt độ < 25°C: Thời tiết "Mát mẻ".
+# Nếu 10°C <= nhiệt độ < 20°C: Thời tiết "Lạnh".
+# Nếu nhiệt độ dưới 10°C: Thời tiết "Rất lạnh".
+# Tư vấn hoạt động:
+
+# Dựa vào trạng thái thời tiết, chương trình sẽ đề xuất một hoạt động phù hợp:
+# "Nóng bức": Khuyên người dùng nên ở trong nhà và sử dụng điều hòa.
+# "Nóng": Khuyên người dùng uống đủ nước và tránh ra ngoài vào buổi trưa.
+# "Ấm": Khuyên người dùng có thể ra ngoài chơi thể thao hoặc dạo bộ.
+# "Mát mẻ": Khuyên người dùng đi dã ngoại hoặc tham gia các hoạt động ngoài trời.
+# "Lạnh": Khuyên người dùng mặc ấm khi ra ngoài.
+# "Rất lạnh": Khuyên người dùng hạn chế ra ngoài, mặc thật ấm và có thể uống nước ấm.
