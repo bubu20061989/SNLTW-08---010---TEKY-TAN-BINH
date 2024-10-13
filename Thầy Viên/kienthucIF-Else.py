0e4b72f7b5383fba94983f29dd95e47f19b184c0
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-13 08:37:55 +0700

    thầy viên

---
+++ Thầy Viên/kienthucIF-Else.py
@@ -0,0 +1,9 @@
+#Kiểm tra 1 số có lớn hơn 0 hay không
+a = int(input('Hãy nhập số a'))
+if a < 0:
+    print("a là số âm")
+else:
+    print("a là số dương")
+
+# if điều-kiện:
+# nếu điều kiện trả về true => chạy câu lệnh trong if
