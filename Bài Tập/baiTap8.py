48ff816ccd36c88d0942b4c6b98279e9977cd010
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-20 08:58:47 +0700

    Thầy Viên giao bài 8

---
+++ Bài Tập/baiTap8.py
@@ -0,0 +1,7 @@
+# Hãy nhập vào 1 số chẵn
+# Nếu nhập vào 1 số lẻ hoặc số nhỏ hơn 0 thì chương trình sẽ yêu cầu nhập lại
+
+# VD kiểm tra chẵn lẽ
+a = 2
+if a % 2 != 0:
+    print("a là số chẵn")
---
+++ Thầy Viên/kienThucWhile.py
@@ -0,0 +1,10 @@
+# While vòng lặp
+
+# Có 2 dạng while
+
+# Vòng lặp while đơn giản:
+
+a = int(input("Nhập vào 1 số dương"))
+while a < 0:
+    a = int(input("Nhập vào 1 số dương"))
+print('Bạn đã nhập đúng số dương')
