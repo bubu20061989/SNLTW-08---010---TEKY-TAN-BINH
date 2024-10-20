8e90db1b96c484e9dad1bbe3d8131748c4323fbd
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-10-20 09:32:55 +0700

    Update baiTap9.py

--- Bài Tập/baiTap9.py
+++ Bài Tập/baiTap9.py
@@ -1,3 +1,6 @@
 #Nhập vào điểm Toán Văn Anh 
-# kiểm tra điểm < 0 thì nhập lại 
-# tính điểm trung bình môn
+# kiểm tra điểm từng môn học nếu điểm môn đó < 0 hoặc lớn hơn 11 thì nhập lại 
+# tính điểm trung bình 
+# ví du
+
+# diemToan = 10 thì thông báo : Điểm toán sai vui lòng nhâp lại
