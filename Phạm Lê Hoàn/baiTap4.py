dfeb9b77418d536707b09f0ce2857e30b35ff72e
gojonahidwin <lngochieu224@gmail.com>
2024-10-26 22:09:29 +0700

    baiTap4 Phạm Lê Hoàn lần 1
    baiTap4

---
+++ Phạm Lê Hoàn/baiTap4.py
@@ -0,0 +1,4 @@
+soTienGuiLa=float(input('Số tiền bạn muốn gửi là: '))
+lai=float (input('Nhập lãi: '))
+soThangGuiLa=float(input('Số tháng mà bạn gửi là: '))
+print('Tổng số tiền gửi sau 12 tháng là:',soTienGuiLa*(1+lai)*soThangGuiLa)
