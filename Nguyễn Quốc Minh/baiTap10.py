8cb91cbd8bfd87638ae9f1beb62402f0ffde32b4
skibiditoilet999 <minhdoraemon2301@gmail.com>
2024-10-27 07:06:26 +0700

    Quốc Minh nộp baiTap10 lần 1

---
+++ Nguyễn Quốc Minh/baiTap10.py
@@ -0,0 +1,8 @@
+a=float(input('hãy nhập số a'))
+b=float(input('hãy nhập số b'))
+c=float(input('hãy nhập số c'))
+while a > b and a > c:
+    a=float(input('hãy nhập số a'))
+    b=float(input('hãy nhập số b'))
+    c=float(input('hãy nhập số c'))
+print('bạn đã nhập đúng số')
