8166353960136ffec111b7f98cea43ae4098327a
skibiditoilet999 <minhdoraemon2301@gmail.com>
2024-10-27 09:54:58 +0700

    Quốc Minh nộp baiTap11,12,13,14

---
+++ Nguyễn Quốc Minh/baiTap13.py
@@ -0,0 +1,6 @@
+#nhâp một số tính tổng các số từ tổng tới số đó
+tong=0
+motSoBatKi=int(input('hãy nhập số bất kì:'))
+for i in range(motSoBatKi+1):
+    tong=tong+i
+print(tong)
---
+++ Nguyễn Quốc Minh/baiTap14.py
@@ -0,0 +1,6 @@
+tong=0
+motSoBatKi=int(input('hãy nhập số bất kì'))
+for i in range(motSoBatKi+1):
+    if i % 2 ==0:
+        tong=tong+i
+print(tong)
